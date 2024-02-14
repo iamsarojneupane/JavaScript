ab3ec19ed05f8932d5c07fcca0233999638fab38
Saroj Neupane <133830478+iamsarojneupane@users.noreply.github.com>
2024-02-14 15:28:26 +0000

    Conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,37 @@
+// let score = "4JS"
+// let score = null
+// let score = undefined
+// let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score));     // typeof(method)
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// let isLoggedIn = 1   // true
+// let isLoggedIn = 0   // false
+// let isLoggedIn = ""     // false
+let isLoggedIn = "saroj"   // true
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+/* Notes
+"4" => 4
+"4JS" => NaN
+1 => true ; 0 => false;
+"" => false
+"Developer" => true
+
+null => 0
+undefined => undefined
+*/
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
