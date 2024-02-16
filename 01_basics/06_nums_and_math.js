dbb2afc2d7c01da7f2e0ae53800d54e6f24c7cd3
Saroj Neupane <133830478+iamsarojneupane@users.noreply.github.com>
2024-02-16 17:59:27 +0000

    math and nums

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,39 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100.)
+// console.log(balance);
+
+// toString Method
+// console.log(balance.toString().length);
+
+// toFixed Method
+// console.log(balance.toFixed(2));
+
+const otherNumber = 124.9898
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 4000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+/* **************** Maths ****************** */
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 5, 8, 9));
+// console.log(Math.max(4, 3, 6, 9));
+
+console.log(Math.random());  // gives value within 0 and 1.
+console.log(Math.random()*10 + 1);
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) +1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
+
+// const
