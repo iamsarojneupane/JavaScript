a1134ec3a8a29e9d8264e8bdbe2913b5a63d693b
Saroj Neupane <133830478+iamsarojneupane@users.noreply.github.com>
2024-02-27 12:50:50 +0000

    Do-While loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,20 @@
+let i = 0
+while (i <= 10) {
+   // console.log(`Value of i is ${i}`);
+   i = i + 2
+}
+
+let myArray = ["JavaScript", "ReactJS", "TailwindCSS", "NextJS"]
+let arr = 0
+while (arr < myArray.length) {
+   // console.log(`Value is ${myArray[arr]}`);
+   arr++
+}
+
+
+let score = 4
+
+do {
+   console.log(`Score is ${score}`);
+   score++
+} while (score <= 10);
